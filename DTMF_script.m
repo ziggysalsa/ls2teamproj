5c040a4afca0c9c4cf566a751eb4e4d78f8b7032
Nat Rojvachiranonda <rojvachiranonda@gmail.com>
2020-12-04 03:03:59 +0700

    Recording function + test samples
    Added the record function (commented out so we can test by loading the sound files for now). Sound files also created for testing, 6 in total: 3 with noise, 3 without. Had to generate these online and manually add noise cause couldn't find good ones online with 10 digits. 3 varying speeds.

--- DTMF_script.m
+++ DTMF_script.m
@@ -1,1 +1,24 @@
 %This is the script that will run to do the dtmf filtering
+
+close all;
+clear all;
+
+% %Record sound
+% rec = audiorecorder(3000,16,1); %sample rate, bit per sample, channel
+% disp('Press ENTER to start and stop recording');
+% pause;
+% disp('Recording...');
+% record(rec);
+% pause;  %Wait for second ENTER to stop recording
+% stop(rec);
+% disp('Processing...');
+% 
+% %For testing purposes
+% disp('Press ENTER to playback recording');
+% pause;
+% play(rec);
+% 
+% %Recording plot
+% y = getaudiodata(rec);
+% plot(y);
+
