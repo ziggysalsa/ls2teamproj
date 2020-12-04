a669782a5419e86a46d9990e0498bbfee677a3cb
ziggysalsa <54412447+ziggysalsa@users.noreply.github.com>
2020-12-04 09:49:50 -0600

    Update undial.m
    add cd to be able to access testing samples (can change the cd to wherever it is on your machine)

--- undial.m
+++ undial.m
@@ -23,6 +23,10 @@ clear all;
 %-------------------------------------------------------------------------
 
 %Load file
+cd C:\Users\zelen\Documents\GitHub\ls2teamproj %cd to my github directory
+currentFolder = pwd;                                               %get curr folder address
+addpath(append(currentFolder,'\Testing samples\With noise'));   %add folders to file path
+addpath(append(currentFolder, '\Testing samples\Without noise'));   %add folders to file path
 fileName = '1234567890.wav';
 [y,fs] = audioread(fileName);
 fn = fs/2;
