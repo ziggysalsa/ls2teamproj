3db50cc7cc9cfacea85de7846cd37ced99794832
ziggysalsa <54412447+ziggysalsa@users.noreply.github.com>
2020-12-05 12:24:51 -0600

    Make main script
    No code, just rough structure.

---
+++ MainScript.m
@@ -0,0 +1,10 @@
+% Linear Systems II: the Signal Separators
+% This is the main script for our project. 
+
+% Needs to run the record audio function here
+% and return normalized Y and Fs.
+
+% Then, pass Y and Fs to a separate signal function
+% which returns a vector of integers
+
+% Then, call send_text_message.m to send the text.
