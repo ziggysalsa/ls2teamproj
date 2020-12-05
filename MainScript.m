50e8146bff2df8f7bc89b41443dc2b20a0cbe9e8
ziggysalsa <54412447+ziggysalsa@users.noreply.github.com>
2020-12-05 13:55:23 -0600

    Add separateSignal to main file

--- MainScript.m
+++ MainScript.m
@@ -4,14 +4,14 @@
 close all; clear all;
 
 % Needs to run the record audio function here
+% and return normalized Y and Fs.
 input('press Enter to record your dial');
 [y,Fs] = record_audio();
 input('press Enter to play your recording');
 soundsc(y,Fs);
 
-% and return normalized Y and Fs.
-
 % Then, pass Y and Fs to a separate signal function
-% which returns a vector of integers
+% which returns a vector of peaks and their time locations
+[pks, locs] = separateSignal(y);
 
 % Then, call send_text_message.m to send the text.
