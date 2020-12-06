cc929cdf3647336649f044c860d394e041f6366b
Nat Rojvachiranonda <rojvachiranonda@gmail.com>
2020-12-07 02:26:09 +0700

    Update undial.m

--- undial.m
+++ undial.m
@@ -152,21 +152,3 @@ function y = customCheby2(rp,rs,fn,f,x)
     %the second order section as well as the gain which is equivalent to
     %transfer function. Similar to [b,a].
 end
-
-% Function for separating the signal
-% Input 'data' which should be our cleaned signal 'yf' in this case
-% Spit out a 'signal' matrix which contains each separated signal segment..
-% ..in each row of the matrix, the segments are what we will filter
-function signal = separate_Signal(data)
-    [~,locs] = findpeaks(data,'MinPeakDistance',1000,'MinPeakHeight',0.5);  % find important peaks 
-    minLength = 500;                    % set the min length of signal to 2 * 500
-    if locs(1) < minLength                        
-            signal(1,:) = yf(1:1000);
-        else
-            signal(1,:) = yf(locs(1)-minLength : locs(1)+minLength);
-    end
-
-    for i = 2 : length(locs)            % loop over and assign content for each separated signal
-        signal(i,:) = yf(locs(i)-minLength : locs(i)+minLength-1)';
-    end
-end
